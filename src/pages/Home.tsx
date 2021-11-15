0ceb5cf54ec2f26c2930b574a6902a06c04af4b4
Codefique Start <codefiquestart@MacBook-Air-de-Codefique.local>
2021-11-15 09:31:32 -0300

    aula 02

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -7,13 +6,11 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-import { useContext } from 'react';
-import { AuthContext } from '../App';
-
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
   const history = useHistory();
-  const {user, signInWithGoogle} = useContext(AuthContext);
+  const {user, signInWithGoogle} = useAuth();
   
   async function handleCreateRoom() {
     if(!user){
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,11 +5,11 @@ import logoImg from '../assets/images/logo.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-import { useContext } from 'react';
-import { AuthContext } from '../App';
+// import { useAuth } from '../hooks/useAuth';
+
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext);
+  // const { user } = useAuth();
 
   return (
     <div id="page-auth">
@@ -23,7 +22,6 @@ export function NewRoom() {
 
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input
