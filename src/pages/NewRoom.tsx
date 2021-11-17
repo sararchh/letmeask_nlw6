1cd047cd62e39a2861af29c5cc8e49d662dd2857
Codefique Start <codefiquestart@MacBook-Air-de-Codefique.local>
2021-11-17 19:44:46 -0300

    criado as salas

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -28,10 +28,9 @@ export function NewRoom() {
       title: newRoom,
       authorId: user?.id,
     })
-    
-    history.push(`/rooms/${firebaseRoom.key}`);
-  }
 
+    history.push(`/rooms/${firebaseRoom.key}`)
+  }
   return (
     <div id="page-auth">
       <aside>
