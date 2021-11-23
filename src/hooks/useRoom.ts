5b2f59a94df89fbc0beee1a9abe11121b6eda746
Codefique Start <codefiquestart@MacBook-Air-de-Codefique.local>
2021-11-23 20:05:38 -0300

    feito o like

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<string, {
   author: {
@@ -9,6 +10,9 @@ type FirebaseQuestions = Record<string, {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likes: Record<string, {
+    authorId: string;
+  }>
 }>
 
 type QuestionType = {
@@ -20,34 +24,41 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number;
+  hasLiked: boolean;
 }
 
 export function useRoom(roomId: string) {
- 
-    const [questions, setQuestions] = useState<QuestionType[]>([]);
-    const [title, setTitle] = useState('');
+  const { user } = useAuth();
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [title, setTitle] = useState('');
 
-    useEffect(() => {
-      const roomRef = database.ref(`rooms/${roomId}`);
-  
-      roomRef.on('value', room => {
-        const databaseRoom = room.val();
-        const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-  
-        const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-          return {
-            id: key,
-            content: value.content,
-            author: value.author,
-            isHighlighted: value.isHighlighted,
-            isAnswered: value.isAnswered,
-          }
-        })
-  
-        setTitle(databaseRoom.title);
-        setQuestions(parsedQuestions);
-  
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+        }
       })
-    }, [roomId]);
-    return {questions, title}
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    })
+
+    return() => {
+      roomRef.off('value');
+    }
+  }, [roomId, user?.id]);
+  return { questions, title }
 }
